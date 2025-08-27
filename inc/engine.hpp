c1b37a89fd3266f26766ba890e095b90644b7908
Masonlet <masonletoile@hotmail.com>
2025-08-27 19:26:38 -0400

    Switched to CMake, need to switch off of GLM

--- inc/engine.hpp
+++ inc/engine.hpp
@@ -11,12 +11,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-// Math Libraries
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
 // Standard Libraries
 #include <array>
 #include <vector>
